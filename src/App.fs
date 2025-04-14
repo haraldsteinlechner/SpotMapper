d9a0a7e4ce00e379743e182c5a196910e6a11484
Harald Steinlechner <haraldsteinlechner@gmail.com>
2025-04-14 15:08:50 +0200

    updated submit label

--- src/App.fs
+++ src/App.fs
@@ -217,7 +217,7 @@ let render (state: State) (dispatch: Msg -> unit) =
         ]
         Bulma.section [
             Bulma.field.div [
-                Bulma.label "Provide Usage File for 2024 (csv export)"
+                Bulma.label "Provide Usage File for (smartmeter csv export, only 2024 is supported)"
                 Bulma.control.div [
                     FileUpload.createFileUpload2 (dispatch << SetUsage)
                 ]
